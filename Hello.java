418e1c3c5c3e01f45ae35682343019f9d83fb33f
iason <iason.andriopoulos@gmail.com>
2013-07-03 11:18:07 +0200

    YOLO

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
+/* The simplest code you might ever see. */
+
 public class Hello
 {
 	public static void main(String args[])
 	{
 		System.out.println("HELLO");
+
+		/* Say hello to my little friend */
 	}
 }
